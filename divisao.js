0d47763f390b673b38f01ea313b84a63397ee52b
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 14:05:31 -0300

    Delete divisao.js

--- divisao.js
+++
@@ -1,4 +0,0 @@
-var numero = parseInt(prompt("Digite um número"));
-var numero1 = parseInt(prompt("Digite outro número"));
-var divisao = numero / numero1;
-alert("A divisão dos números é: " + divisao)
