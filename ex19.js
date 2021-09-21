e3818f95a0f0115b6bfddc1691d36773ae0b67a9
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:50:06 -0300

    Delete ex19.js

--- ex19.js
+++
@@ -1,9 +0,0 @@
-var num1 = parseInt(prompt("Digite o primeiro valor"));
-var num2 = parseInt(prompt("Digite o segundo valor"));
-
-
-if (num1 > num2) {
-    alert(num1 + " é maior que " + num2);
-} else {
-    alert(num2 + " é maior que " + num1);
-}
