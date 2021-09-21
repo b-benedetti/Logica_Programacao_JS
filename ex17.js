24e2a3a5d48afff566b5809abffe6189dee230f1
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:47:05 -0300

    Delete ex17.js

--- ex17.js
+++
@@ -1,8 +0,0 @@
-var num1 = parseFloat(prompt("Digite a 1°nota"));
-var num2 = parseFloat(prompt("Digite a 2° nota"));
-
-if (((num1 + num2) / 2) >= 6) {
-    alert(" Aprovado : " + ((num1 + num2) / 2));
-} else {
-    alert(" Reprovado : " + ((num1 + num2) / 2));
-}
