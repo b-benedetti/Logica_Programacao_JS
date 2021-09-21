12c77ac0d06568eb5c28b623570fa6e3013df7a0
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:08:20 -0300

    Delete ex56.js

--- ex56.js
+++
@@ -1,9 +0,0 @@
-var  num  =   (prompt("Digite um número: "));
-var  cont  =  1;
-let  texto  =  "";
-while  (cont  <=  10)  {    
-    var  resultado  =  num  *  cont;    
-    texto  +=  "<br>"  +  num  +  " * "  +  cont  +  " = "  +  resultado;    
-    cont++;
-}
-document.getElementById("demo").innerHTML  =  texto;
