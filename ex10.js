7cf83a536454bc09caf1fc4ab9076f45a27e2877
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:42:40 -0300

    Delete ex10.js

--- ex10.js
+++
@@ -1,5 +0,0 @@
-var custoFab = parseFloat(prompt("Digite o custo de fábrica."));
-var porcDist;
-var impostos;
-var custoFinal = custoFab + (28 / 100 * custoFab) + (45 / 100 * custoFab);
-alert("O custo final do carro será de R$ : " + custoFinal);
