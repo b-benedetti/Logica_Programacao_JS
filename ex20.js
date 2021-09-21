809c791f63072f6dab697f65de068aa620ff7807
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:50:23 -0300

    Delete ex20.js

--- ex20.js
+++
@@ -1,9 +0,0 @@
-var num1 = parseInt(prompt("Digite o primeiro valor"));
-var num2 = parseInt(prompt("Digite o segundo valor"));
-
-
-if (num1 > num2) {
-    alert(num2 + " e " + num1);
-} else {
-    alert(num1 + " e " + num2);
-}
