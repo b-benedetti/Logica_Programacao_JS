c7099e6208ea810099b27323139eb68f3093c041
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 14:05:53 -0300

    Delete subtracao.js

--- subtracao.js
+++
@@ -1,4 +0,0 @@
-var numero = parseInt(prompt("Digite um número"));
-var numero1 = parseInt(prompt("Digite outro número"));
-var subtracao = numero - numero1;
-alert("A subtração dos números é: " + subtracao)
