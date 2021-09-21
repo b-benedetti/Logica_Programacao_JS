a617f957f7fb477fe8da2080e947ec8e71b9cfef
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:45:29 -0300

    Delete ex13.js

--- ex13.js
+++
@@ -1,9 +0,0 @@
-var N1 = parseInt(prompt("Digite a primeira nota: "));
-
-var N2 = parseInt(prompt("Digite a segunda nota: "));
-
-var N3 = parseInt(prompt("Digite a terceira nota: "));
-
-notaFinal = (N1 * 2 + N2 * 3 + N3 * 5) / 10;
-
-alert("A média ponderada do aluno é: " + notaFinal);
