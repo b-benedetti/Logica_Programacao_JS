c842783242c942bc810671a33c332db36f0d6974
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:46:52 -0300

    Delete ex16.js

--- ex16.js
+++
@@ -1,7 +0,0 @@
-var num = parseInt(prompt("Digite o número de maçãs compradas"));
-
-if (num < 12 & num > 0) {
-    alert(" Valor final: 1,30 * " + num + " = R$ " + num * 1.30);
-} else {
-    alert(" Valor final: 1,00 * " + num + " = R$ " + num);
-}
