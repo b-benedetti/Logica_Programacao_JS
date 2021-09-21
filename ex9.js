39753691559c4f6a4d9988b6676d43ff2a387f5c
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 14:01:27 -0300

    Delete ex9.js

--- ex9.js
+++
@@ -1,5 +0,0 @@
-var sal = parseFloat(prompt("Digite o seu salário mensal"));
-var reaj = parseFloat(prompt("Digite a porcentagem do reajuste"));
-var saltotal;
-saltotal = sal * reaj / 100 + sal;
-alert("O seu salário será de R$ " + saltotal);
