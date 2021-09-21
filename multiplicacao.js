5ae17a1ea0d26066f3569071a9d92ed00d69b426
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 14:05:42 -0300

    Delete multiplicacao.js

--- multiplicacao.js
+++
@@ -1,4 +0,0 @@
-var numero = parseInt(prompt("Digite um número"));
-var numero1 = parseInt(prompt("Digite outro número"));
-var multiplicacao = numero * numero1;
-alert("A multiplicação dos números é: " + multiplicacao)
