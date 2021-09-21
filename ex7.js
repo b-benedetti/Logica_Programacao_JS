39d9b937bd689f8ae56b99a60c763aa3991a31d7
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 14:02:03 -0300

    Delete ex7.js

--- ex7.js
+++
@@ -1,6 +0,0 @@
-var anos = parseInt(prompt("Digite sua idade em anos"));
-var meses = parseInt(prompt("Digite sua idade em meses"));
-var dias = parseInt(prompt("Digite sua idade em dias"));
-anos = anos * 365;
-meses = meses * 30;
-alert("Sua idade em dias é : " + (anos + meses + dias));
