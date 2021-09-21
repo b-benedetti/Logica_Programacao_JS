39be228b2698daceb30272155a5872f70cf9f5ef
Barbara Benedetti <86322587+b-benedetti@users.noreply.github.com>
2021-09-21 13:45:15 -0300

    Delete ex12.js

--- ex12.js
+++
@@ -1,5 +0,0 @@
-var grausF = parseFloat(prompt("Digite a temperatura em graus Fahrenheit:"));
-
-let grausC = (grausF - 32) * 5 / 9;
-
-alert("a temperatura em graus Celsius é:" + grausC.toFixed(2))
